cc7ff31074d5803e4607b33e5900152d570ee98f
rebecca burch <rebecca.burch.stu@gmail.com>
2020-11-07 16:25:58 -0600

    firebase addition

---
+++ build/precache-manifest.5bb9371d297382ff256fb86b62f92f9d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d743eba1819938e7d0d5d3cc1e5492",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96a1df435fe0645fca67",
+    "url": "/static/css/main.1708f7b9.chunk.css"
+  },
+  {
+    "revision": "af930befdec24e0a535f",
+    "url": "/static/js/2.79b3074e.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/static/js/2.79b3074e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a1df435fe0645fca67",
+    "url": "/static/js/main.7396405b.chunk.js"
+  },
+  {
+    "revision": "3974a6a99cc8d61491f1",
+    "url": "/static/js/runtime-main.89156b78.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "950b95fcf7180bcc1a1545af26cb7537",
+    "url": "/static/media/htmlcss.950b95fc.png"
+  },
+  {
+    "revision": "f364bdab265082e6ad5ad47f3a83beda",
+    "url": "/static/media/javascript.f364bdab.png"
+  },
+  {
+    "revision": "7d347d6cf6c969d85d575898deb062c5",
+    "url": "/static/media/mern-stack.7d347d6c.png"
+  },
+  {
+    "revision": "ed585beb4caa98d413d6c717ae8ebd6a",
+    "url": "/static/media/react1.ed585beb.png"
+  }
+]);
